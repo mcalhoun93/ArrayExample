0f378bacd30128244df033a3c964c6b6a10f3706
Michael Calhoun <michael.calhoun91@gmail.com>
2014-05-23 11:40:59 -0500

    First Commit

---
+++ Calhoun_M Chap 8.3 EX/Calhoun_m Chap 8.3 EX/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TicketSales
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TotalSalesReportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TicketsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ShowPricesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PurchaseTicketsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearSelectionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.tixtxtbox = New System.Windows.Forms.TextBox()
+        Me.amtduetxtbox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.TicketsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(470, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TotalSalesReportToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'TotalSalesReportToolStripMenuItem
+        '
+        Me.TotalSalesReportToolStripMenuItem.Name = "TotalSalesReportToolStripMenuItem"
+        Me.TotalSalesReportToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.TotalSalesReportToolStripMenuItem.Text = "Total Sales Report"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(165, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(168, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'TicketsToolStripMenuItem
+        '
+        Me.TicketsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ShowPricesToolStripMenuItem, Me.PurchaseTicketsToolStripMenuItem, Me.ClearSelectionsToolStripMenuItem})
+        Me.TicketsToolStripMenuItem.Name = "TicketsToolStripMenuItem"
+        Me.TicketsToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.TicketsToolStripMenuItem.Text = "Tickets"
+        '
+        'ShowPricesToolStripMenuItem
+        '
+        Me.ShowPricesToolStripMenuItem.Name = "ShowPricesToolStripMenuItem"
+        Me.ShowPricesToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.ShowPricesToolStripMenuItem.Text = "Show Prices"
+        '
+        'PurchaseTicketsToolStripMenuItem
+        '
+        Me.PurchaseTicketsToolStripMenuItem.Name = "PurchaseTicketsToolStripMenuItem"
+        Me.PurchaseTicketsToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.PurchaseTicketsToolStripMenuItem.Text = "Purchase Tickets"
+        '
+        'ClearSelectionsToolStripMenuItem
+        '
+        Me.ClearSelectionsToolStripMenuItem.Name = "ClearSelectionsToolStripMenuItem"
+        Me.ClearSelectionsToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.ClearSelectionsToolStripMenuItem.Text = "Clear Selections"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.tixtxtbox)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(35, 88)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(219, 166)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select Section and Number of Tickets"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(46, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Section:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(123, 38)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Number:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(298, 142)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Amount Due:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Orchestra", "Mezzanine", "General", "Balcony"})
+        Me.ListBox1.Location = New System.Drawing.Point(6, 54)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(76, 56)
+        Me.ListBox1.TabIndex = 2
+        '
+        'tixtxtbox
+        '
+        Me.tixtxtbox.Location = New System.Drawing.Point(116, 70)
+        Me.tixtxtbox.Name = "tixtxtbox"
+        Me.tixtxtbox.Size = New System.Drawing.Size(62, 20)
+        Me.tixtxtbox.TabIndex = 3
+        '
+        'amtduetxtbox
+        '
+        Me.amtduetxtbox.Location = New System.Drawing.Point(286, 177)
+        Me.amtduetxtbox.Name = "amtduetxtbox"
+        Me.amtduetxtbox.ReadOnly = True
+        Me.amtduetxtbox.Size = New System.Drawing.Size(100, 20)
+        Me.amtduetxtbox.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(114, 44)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(231, 25)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Concert Ticket Sales"
+        '
+        'TicketSales
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 355)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.amtduetxtbox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "TicketSales"
+        Me.Text = "Ticket Sales"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TotalSalesReportToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TicketsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ShowPricesToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PurchaseTicketsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearSelectionsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents tixtxtbox As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents amtduetxtbox As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+
+End Class
---
+++ Calhoun_M Chap 8.3 EX/Calhoun_m Chap 8.3 EX/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class TicketSales
+
+    'Private totmezdecimal, totodecimal, totgdecimal, totbaldecimal, totsaledecimal As Decimal
+    'Private totmeztic, tototic, totgtic, totbaltic As Integer
+    Private totalsalesdecimal(3) As Decimal
+    Private amountsoldinteger(3) As Integer
+    Private pricedecimal() As Decimal = {40D, 27.5D, 15D, 10D}
+
+    Private Sub ShowPricesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShowPricesToolStripMenuItem.Click
+
+        TicketPrices.ShowDialog()
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub TotalSalesReportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles TotalSalesReportToolStripMenuItem.Click
+
+        Dim messagestring As String = String.Empty
+        Dim sectionstring As String
+
+        For sectioninteger As Integer = 0 To 3
+
+            sectionstring = ListBox1.Items(sectioninteger) & " (" & pricedecimal(sectioninteger).ToString("C") & " per ticket): " & _
+                           amountsoldinteger(sectioninteger) & " sold " & totalsalesdecimal(sectioninteger).ToString("C") & _
+                           Environment.NewLine & Environment.NewLine
+
+            messagestring += sectionstring
+        Next
+
+        MessageBox.Show(messagestring, "Sales Summary", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+
+    End Sub
+
+    Private Sub ClearSelectionsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearSelectionsToolStripMenuItem.Click
+
+        amtduetxtbox.Clear()
+        tixtxtbox.Clear()
+        ListBox1.SelectedIndex = -1
+
+    End Sub
+
+    Private Sub PurchaseTicketsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PurchaseTicketsToolStripMenuItem.Click
+
+        Dim numbertixinteger, indexinteger As Integer
+        Dim totaldecimal As Decimal
+
+        If ListBox1.SelectedIndex <> -1 Then
+
+            Try
+                numbertixinteger = Integer.Parse(tixtxtbox.Text)
+            Catch ex As Exception
+                MessageBox.Show("Please Enter a Valid Number of Tickets.", "Data Entry Error", _
+                                MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+
+                With tixtxtbox
+                    .Focus()
+                    .SelectAll()
+                End With
+
+            End Try
+
+            indexinteger = ListBox1.SelectedIndex
+            totaldecimal = pricedecimal(indexinteger) * numbertixinteger
+
+            amtduetxtbox.Text = totaldecimal.ToString("C")
+
+            totalsalesdecimal(indexinteger) += totaldecimal
+            amountsoldinteger(indexinteger) += numbertixinteger
+
+        Else
+            MessageBox.Show("Please Select a Seating Section.", "Data Entry Error", _
+                            MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+
+    End Sub
+End Class
